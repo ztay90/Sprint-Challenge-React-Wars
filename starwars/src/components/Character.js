753b06998f6b6f665fa2d383b76e331f27ce49aa
Zachary Taylor <ztay90@gmail.com>
2020-05-03 20:50:16 -0400

    Finished Project and Answers.md

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,34 @@
-// Write your Character component here
+import React, { useState, useEffect} from "react";
+import axios from "axios";
+import CharacterContainer from "./CharacterContainer";
+
+const Character = () => {
+
+  const[data, setData] = useState([])
+
+  useEffect(() => {
+    axios.get("https://rickandmortyapi.com/api/character/")
+      .then( response => {
+        setData(response.data.results)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+  return (
+    <div>
+      {data.map(character => {
+        return(
+          <CharacterContainer
+            name = {character.name}
+            species = {character.species}
+            image = {character.image}
+          />
+        )
+        
+      })}
+    </div>
+  )
+}
+
+export default Character
